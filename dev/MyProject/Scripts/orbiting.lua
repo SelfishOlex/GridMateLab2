2964296f29e79e0d2c8855a45bdd6f17dae580d1
Olex Lozitskiy <olex.developer@gmail.com>
2018-08-13 22:46:40 -0700

    Whitespace cleanup.

--- dev/MyProject/Scripts/orbiting.lua
+++ dev/MyProject/Scripts/orbiting.lua
@@ -1,29 +1,29 @@
--- ScriptName.lua -- Constant turning around Z axis 
-local Orbiting = 
+-- ScriptName.lua -- Constant turning around Z axis
+local Orbiting =
 {
     Properties =
     {
         -- Property definitions
-		speed = { default = 10, 
-			description = "speed of orbiting", 
-			suffix = "deg/sec" }
+        speed = { default = 10,
+            description = "speed of orbiting",
+            suffix = "deg/sec" }
     }
 }
 
 function Orbiting:OnActivate()
      -- Activation Code
-	Debug.Log("Orbiting activated")
-	self.tickHandler = TickBus.Connect(self)
+    Debug.Log("Orbiting activated")
+    self.tickHandler = TickBus.Connect(self)
 end
 
 function Orbiting:OnDeactivate()
     -- Deactivation Code
-	self.tickHandler:Disconnect()
+    self.tickHandler:Disconnect()
 end
 
 function Orbiting:OnTick(deltaTime, timePoint)
-	TransformBus.Event.RotateAroundLocalZ(
-		self.entityId, self.Properties.speed*deltaTime)
+    TransformBus.Event.RotateAroundLocalZ(
+        self.entityId, self.Properties.speed*deltaTime)
 end
 
 return Orbiting
